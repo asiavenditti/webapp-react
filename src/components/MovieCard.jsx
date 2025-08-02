49792589b800af6a6b98260337b2d517eb7e90c1
Your Name <you@example.com>
2025-08-02 13:01:37 +0200

    Refactor MovieCard and HomePage components to display movie details dynamically

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,20 @@
+import { Link } from 'react-router-dom'
 
-export default function MovieCard() {
+export default function MovieCard({ movie }) {
     return (
-
-        <h1>Movie Card</h1>
+        <div className="card h-100">
+            <img
+                src={movie.image}
+                className="card-img-top"
+                alt={movie.title}
+            />
+            <div className="card-body d-flex flex-column">
+                <h5 className="card-title">{movie.title}</h5>
+                <p className="card-text">Anno: {movie.release_year}</p>
+                <Link to={`/movie/${movie.id}`} className="btn btn-primary mt-auto">
+                    Scopri di più
+                </Link>
+            </div>
+        </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,28 @@
-import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import MovieCard from '../components/MovieCard'
+
 export default function HomePage() {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        const url_movies = 'http://localhost:3000/api/movies'
+        fetch(url_movies)
+            .then((res) => res.json())
+            .then((data) => setMovies(data))
+            .catch((err) => console.error('Errore', err))
+    }, []);
+
     return (
-        <h1>Homepage</h1>
+        <div className="container mt-4">
+            <h1 className="mb-4">🎬 Lista Film</h1>
+            <div className="row">
+                {movies.map((movie) => (
+                    <div className="col-md-4 mb-4" key={movie.id}>
+                        <MovieCard movie={movie} />
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
+
